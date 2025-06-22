41a8ff94603f2114e383771c66f3860651809794
Byumin <qndbals@1023@gmail.com>
2025-06-22 17:17:22 +0900

    mac upload

--- model_sketch/click_coords.py
+++ model_sketch/click_coords.py
@@ -4,6 +4,7 @@ import json
 
 def click_coords(image_path, save_path="coords.json"):
     coords = []  # 클릭된 좌표를 저장할 리스트
+    labeled_bbox = [] # 리스트로 딕셔너리(객체)들 묶기
 
     # 클릭 이벤트에 반응하는 인터렉션을 정의하는 함수
     def onclick(event):
@@ -17,7 +18,12 @@ def click_coords(image_path, save_path="coords.json"):
             if len(coords) == 2 :
                 print("2개의 점이 지정되었습니다. 해장 박스의 클래스명을 입력해주세요.")
                 label = input("클래스명 (예 : bar, line, table) : ").strip()
-                label #### 어떠한 구조로 저장할지 검토 중 25.06.17
+                labeled_bbox.append({
+                    "label" : label,
+                    "bbox" : coords.copy()
+                })
+                coords.clear()
+                
     img = mpimg.imread(image_path)
     fig, ax = plt.subplots()
     ax.imshow(img)
@@ -31,8 +37,8 @@ def click_coords(image_path, save_path="coords.json"):
     # re : 이벤트 리스터를 명시해주고 plt.show() 실행하는 순간 이벤트 리스터는 동작하게 되어 있음.
     # re : 즉 이벤트 리스터를 동작시켜라 라는 의미의 신호 같은 역할 + 시각화 플롯을 열어주는 역할
     with open(save_path, "w") as f :
-        json.dump(coords, f)
-    print(f"총 {len(coords)}개의 좌표가 저장되었습니다 -> {save_path}")
+        json.dump(labeled_bbox, f)
+    print(f"라벨과 좌표가 저장되었습니다 -> {save_path}")
 
     return coords
 
